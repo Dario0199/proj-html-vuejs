e29b581b224ad59419d23ca8fce3ed1a6d043992
Dario0122 <dario.di.cuia@gmail.com>
2021-12-11 18:41:04 +0100

    section process management

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -17,16 +17,83 @@
                 Company
             </h1>
             <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras iaculis diam varius diam ultricies lacinia.</span>
-            <div class="d-flex mt-5">
+            <div class="d-flex mt-3">
                 <div v-for="(card, index) in cardsCompany" :key="`company-${index}`" class="card-company">
                     <h2>{{ card.number }}</h2>
                     <h4>{{ card.title }}</h4>
                     <span>{{ card.text }}</span>
                 </div>
             </div>
+            <button class="b-left">get in touch</button>
+            <button class="b-right">read more</button>
         </div>
-        
         <img src="../assets/about-5.jpg" alt="">
+    </div>
+    <div class="projects">
+        <div class="container d-flex flex-column align-items-center">
+            <h5>we do more for everyone</h5>
+            <h1 class="d-flex">
+                Actions & 
+                <div class="h1-bg">Projects</div> 
+            </h1>
+            <div class="menu-bar">
+                <ul class="link">
+                    <li v-for="(link, index) in menuLinks" :key="`link-${index}`">
+                        <a :href="link.url">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="projercts-card">
+                <div class="card">
+                    <img src="../assets/project-1.jpg" alt="">
+                    <h5>Academic professional program in social media</h5>
+                </div>
+                <div class="card">
+                    <img src="../assets/project-2.jpg" alt="">
+                    <h5>President's speech at the annual meeting</h5>
+                </div>
+                <div class="card">
+                    <img src="../assets/project-3.jpg" alt="">
+                    <h5>International business trip in Shanghai</h5>
+                </div>
+                <div class="card">
+                    <img src="../assets/project-4.jpg" alt="">
+                    <h5>Technology workshop with education theme</h5>
+                </div>
+                <div class="card">
+                    <img src="../assets/project-5.jpg" alt="">
+                    <h5>Donation of clothes and food to the partner NGO</h5>
+                </div>
+                <div class="card">
+                    <img src="../assets/project-6.jpg" alt="">
+                    <h5>Confraternization of the procurement team</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="management d-flex align-items-center flex-column">
+        <h5>how it works in practice</h5>
+        <h1 class="d-flex">
+            <div class="h1-bg">
+                Process
+            </div>
+            Menagement
+        </h1>
+        <span>We work with innovative methodologies to ensure that the entire reformatting process is done from start to finish as planned.</span>
+        <div class="point-bar">
+            <div class="bar">
+                <div class="content d-flex justify-content-around">
+                <div v-for="(card, index) in pointCards" :key="`point-${index}`" class="cards">
+                    <div class="d-flex justify-content-center">
+                        <h5>{{ card.number }}</h5>
+                    </div>
+                    <h4>{{ card.title }}</h4>
+                    <span>{{ card.text }}</span>
+                </div>
+            </div>
+            </div>
+            
+        </div>
     </div>
   </main>
 </template>
@@ -37,41 +104,123 @@ export default {
     data(){
         return{
             cards: [
-            {
-                icon: 'fas fa-user-friends',
-                title: 'Human Capital',
-                text: 'Humanizing business: Harness the power of technology to improve the way people work.'
-            },
-            {
-                icon: 'fas fa-chart-pie',
-                title: 'Core Business',
-                text: 'It takes innovative approaches to transform, modernize, and run existing platforms.'
-            },
-            {
-                icon: 'fab fa-cloudscale',
-                title: 'Performance',
-                text: 'Achieving maximum impact and value from investments in finance and supply chain.'
-            }
-        ],
-        cardsCompany: [
-            {
-                number: 0,
-                title: 'Employees',
-                text: 'Lorem ipsum dolor sit amet'
-            },
-            {
-                number: 0,
-                title: 'Partners',
-                text: 'Lorem ipsum dolor sit amet'
-            },
-            {
-                number: 0,
-                title: 'Customers',
-                text: 'Lorem ipsum dolor sit amet'
-            },
-        ]
+                {
+                    icon: 'fas fa-user-friends',
+                    title: 'Human Capital',
+                    text: 'Humanizing business: Harness the power of technology to improve the way people work.'
+                },
+                {
+                    icon: 'fas fa-chart-pie',
+                    title: 'Core Business',
+                    text: 'It takes innovative approaches to transform, modernize, and run existing platforms.'
+                },
+                {
+                    icon: 'fab fa-cloudscale',
+                    title: 'Performance',
+                    text: 'Achieving maximum impact and value from investments in finance and supply chain.'
+                }
+            ],
+            cardsCompany: [
+                {
+                    number: 0,
+                    title: 'Employees',
+                    text: 'Lorem ipsum dolor sit amet'
+                },
+                {
+                    number: 0,
+                    title: 'Partners',
+                    text: 'Lorem ipsum dolor sit amet'
+                },
+                {
+                    number: 0,
+                    title: 'Customers',
+                    text: 'Lorem ipsum dolor sit amet'
+                },
+            ],
+            menuLinks:[
+                {
+                    text: 'all',
+                    url: '/',
+                },
+                {
+                    text: 'institutional',
+                    url: '/',
+                },
+                {
+                    text: 'social',
+                    url: '/',
+                },
+                {
+                    text: 'events',
+                    url: '/',
+                },
+                {
+                    text: 'innovation',
+                    url: '/',
+                },
+                    {
+                    text: 'environment',
+                    url: '/',
+                },
+                    {
+                    text: 'technology',
+                    url: '/',
+                },
+            ],
+            // projectCards:[
+            //     {
+            //         image: '1',
+            //         text: 'Academic professional program in social media'
+            //     },
+            //     {
+            //         image: '2',
+            //         text: "President's speech at the annual meeting"
+            //     },
+            //     {
+            //         image: '3',
+            //         text: 'International business trip in Shanghai'
+            //     },
+            //     {
+            //         image: '4',
+            //         text: 'Technology workshop with education theme'
+            //     },
+            //     {
+            //         image: '5',
+            //         text: 'Donation of clothes and food to the partner NGO'
+            //     },
+            //     {
+            //         image: '6',
+            //         text: 'Confraternization of the procurement team'
+            //     }
+            // ]
+            pointCards:[
+                {
+                    number: '01',
+                    title: 'Collection of information',
+                    text: 'Lorem ipsum dolor sit amet consectetur'
+                },
+                {
+                    number: '02',
+                    title: 'Strategic planning',
+                    text: 'Lorem ipsum dolor sit amet consectetur'
+                },
+                {
+                    number: '03',
+                    title: 'assignment of resposibilities',
+                    text: 'Lorem ipsum dolor sit amet consectetur'
+                },
+                {
+                    number: '04',
+                    title: 'Formatting process',
+                    text: 'Lorem ipsum dolor sit amet consectetur'
+                },
+                {
+                    number: '05',
+                    title: 'Continuity formalization',
+                    text: 'Lorem ipsum dolor sit amet consectetur'
+                }
+            ]
         }
-        
     }
 }
 </script>
@@ -119,15 +268,16 @@ export default {
             text-transform: uppercase;
             font-size: 14px;
             font-weight: 600;
-            margin-bottom: 30px;
+            margin-bottom: 20px;
         }
         h1{
             color: black;
             font-weight: 800;
             font-size: 50px;
-            margin-bottom: 30px;
+            margin-bottom: 10px;
 
             .h1-bg{
+                padding-left: 10px;
                 color: #038483;
                 background: #03848441;
                 width: 110px;
@@ -151,12 +301,187 @@ export default {
                 font-weight: 600;
             }
         }
-
+        button{
+            text-transform: uppercase;
+            margin-top: 30px;
+            margin-right: 15px;
+            color: white;
+        }
+            .b-left{
+                border: none;
+                background: #1a948f;
+                border-radius: 3px;
+                width: 130px;
+                height: 35px;
+            }
+            .b-right{
+                width: 130px;
+                height: 35px;
+                background: none;
+                color: #1a948f;
+                border-radius: 3px;
+                border: 2px solid #1a948f;
+            }
     }
     img{
         width: 50%;
-        height: 500px;
+        height: 550px;
         object-fit: cover;
+    }
+}
+.projects{
+    background: black;
+
+    .container{
+        padding-top: 80px;
+        h5{
+            color: #1a948f;
+            text-transform: uppercase;
+            font-size: 14px;
+            font-weight: 600;
+            margin-bottom: 30px;
+        }
+        h1{
+            color: white;
+            font-weight: 800;
+            font-size: 50px;
+            margin-bottom: 30px;
+            
+            .h1-bg{
+                padding-left: 10px;
+                background: #03848441;
+                width: 220px;
+                margin-left: 10px;
+            }
+        }
+        .menu-bar{
+            margin-bottom: 30px;
+            ul{
+                display: flex;
+                align-items: center;
+                list-style: none;
+                margin-top: 20px;
+                li{
+                    margin-left: 40px;
+                    a{
+                        text-decoration: none;
+                        text-transform: uppercase;
+                        color: white;
+                        font-size: 14px;
+                        font-weight: 600;
+                        &:hover{
+                            background: #03848441;
+                            height: 25px;
+                        }
+                    }
+                }
+            }
+        }
+        .projercts-card{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            .card{
+                width: 400px;
+                border: none;
+                margin-bottom: 45px;
+                background: none;
+                &::after{
+                    position: absolute;
+                    content: '';
+                    width: 100%;
+                    height: 100%;
+                    background-color: black;
+                    opacity: 0.4;
+                    z-index: 0;
+                }
+
+                img{
+                    position: relative;
+                    border-radius: 20px;
+                }
+                h5{
+                    position: absolute;
+                    text-align: center;
+                    color: white;
+                    z-index: 5;
+                    left: 50%;
+                    transform: translateX(-50%);
+                    bottom: 15px;
+                }
+            }
+        }
+    }
+}
+.management{
+    h5{
+        color: #038483;
+        text-transform: uppercase;
+        font-size: 14px;
+        font-weight: 600;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+    h1{
+        color: black;
+        font-weight: 800;
+        font-size: 50px;
+        margin-bottom: 30px;
+        
+        .h1-bg{
+            color: #038483;
+            padding-left: 15px;
+            background: #03848441;
+            width: 220px;
+            margin-right: 10px;
+        }
+    }
+    span{
+        text-align: center;
+        font-size: 16px;
+        color: #85878b;
+        margin-bottom: 50px;
+        width: 700px;
+    }
+    .point-bar{
+        width: 100%;
+        .bar{
+            width: 100%;
+            height: 4px;
+            background-color: #03848441;
+            position: relative;
+        }
+        .content{
+            position: absolute;
+            top: -45px;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 100%;
+            .cards{
+            width: 15%;
+            text-align: center;
+            
+                h5{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background: #bee0e0;
+                    color: #038483;
+                    font-weight: 700;
+                    border-radius: 50%;
+                    width: 35px;
+                    height: 35px;
+                    margin-left: 10px;
+                    margin-bottom: 20px;
+                    text-align: center;
+                }
+                h4{
+                    text-align: center;
+                    margin-bottom: 20px;
+                    font-weight: 600;
+                }
+            }
+        }
     }
 }
 
